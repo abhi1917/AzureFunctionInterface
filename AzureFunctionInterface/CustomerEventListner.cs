0db5fcc28366856e90e88f7bfeb780600b86882f
abhi1917 <abhijeeth1907@gmail.com>
2020-05-06 12:56:11 +0000

    Initial

---
+++ AzureFunctionInterface/CustomerEventListner.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AzureFunctionInterface
+{
+    public static class CustomerEventListner
+    {
+        public class CustomerSendEvent
+        {
+            public string CustomerId { get; set; }
+            public string AgentId { get; set; }
+        }
+
+        [FunctionName("CustomerEventListner")]
+        public static async Task Run([EventHubTrigger("%EventHubName%", Connection = "EventHub")] EventData[] events, ILogger log)
+        {
+            HttpClient httpClient = new HttpClient();
+            try
+            {
+                foreach (EventData eventData in events)
+                {
+                    string value = Encoding.UTF8.GetString(eventData.Body);
+                    CustomerSendEvent customer = new CustomerSendEvent
+                    {
+                        CustomerId = value,
+                        AgentId = "EventHub"
+                    };
+                    var url = System.Environment.GetEnvironmentVariable("apiEventInvokeurl");
+                    var content = JsonConvert.SerializeObject(customer);
+                    if (null != content)
+                    {
+                        var stringContent = new StringContent(content, UnicodeEncoding.UTF8, "application/json");
+                        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", System.Environment.GetEnvironmentVariable("apiAuthToken"));
+                        var result = await httpClient.PostAsync(url, stringContent);
+                    }
+                    else
+                    {
+                        throw new Exception("Failed to serialize object!");
+                    }
+                    log.LogInformation(value);
+                }
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex.Message);
+            }
+        }
+    }
+}
